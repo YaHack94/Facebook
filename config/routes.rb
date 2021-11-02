747576c54025ce2a803af55458f9deff3285af73
YaHack94 <yahack94@gmail.com>
2021-11-02 22:04:06 +0100

    Publication update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/', to: 'generals#index', as: 'index'
   get '/sign_up', to: 'accounts#sign_up', as: 'signup'
   get 'publication/:id/edit', to: 'accounts#edit', as: 'editform'
+  post 'publication/:id/edit', to: 'accounts#edit'
   post 'publication/:id/edit', to: 'accounts#update', as: 'modifier'
   #post '/publication_edited/:id', to: 'accounts#update', as: 'editform'
   post '/sign_up_form', to: 'accounts#sign_up_form', as: 'signupform'
