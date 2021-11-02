3c6e047fc9f8f977cf931c56f3ae09caa4612ea7
YaHack94 <yahack94@gmail.com>
2021-11-02 09:59:31 +0100

    Connexion

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -23,11 +23,28 @@ class AccountsController < ApplicationController
 
 	end
 
+	def login
+
+		@user = User.find_by(email:login_params[:email])
+    if @user && @user.authenticate(login_params[:password])
+      session[:current_user_id] = @user.id
+      redirect_to '/dashboard'
+    else
+      flash[:login_errors] = ['User Name or Password is not correct']
+      redirect_to '/'
+    end
+
+	end
+
 
 	private
 
 		def user_params
 			params.require(:user).permit(:last_name, :first_name, :email, :gender, :date_of_birth, :password)
 		end
-	
+
+		def login_params
+      params.require(:login).permit(:email, :password )
+    end
+
 end
