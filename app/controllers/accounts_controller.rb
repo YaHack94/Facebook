52fda319ab7be7c4f3e9a84702f52661adaede23
YaHack94 <yahack94@gmail.com>
2021-11-02 09:58:02 +0100

    Inscription

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -9,4 +9,25 @@ class AccountsController < ApplicationController
 
 	end
 
+	def sign_up_form
+
+		@user = User.new(user_params)
+
+    if @user.save
+      session[:current_user_id] = @user.id
+      redirect_to '/dashboard'
+    else
+      flash[:register_errors] = @user.errors.full_messages
+      redirect_to '/sign_up'
+    end
+
+	end
+
+
+	private
+
+		def user_params
+			params.require(:user).permit(:last_name, :first_name, :email, :gender, :date_of_birth, :password)
+		end
+	
 end
